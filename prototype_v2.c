98868f05f3980a3c3767874afd73b24460cbe65e
Michael Rouves <mrouves@student.42angouleme.fr>
2025-01-14 20:53:49 +0100

    patched double linked list, started uint64_map

--- prototype_v2.c
+++ prototype_v2.c
@@ -6,7 +6,7 @@
 /*   By: mykle <mykle@42angouleme.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 12:02:52 by mykle             #+#    #+#             */
-/*   Updated: 2025/01/13 20:48:16 by mykle            ###   ########.fr       */
+/*   Updated: 2025/01/14 20:52:25 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,18 +60,17 @@ void	dl_list_insert(t_dl_list **after, t_dl_list *node)
 		node->next->prev = node;
 }
 
-void	dl_list_delone(t_dl_list **node, void (*del)(void *))
+void	dl_list_delone(t_dl_list *node, void (*del)(void *))
 {
-	if (__builtin_expect(!node || !(*node), 0))
+	if (__builtin_expect(!node, 0))
 		return ;
-	if ((*node)->prev)
-		(*node)->prev->next = (*node)->next;
-	if ((*node)->next)
-		(*node)->next->prev = (*node)->prev;
+	if (node->prev)
+		node->prev->next = node->next;
+	if (node->next)
+		node->next->prev = node->prev;
 	if (del)
-		del((*node)->data);
-	free(*node);
-	*node = NULL;
+		del(node->data);
+	free(node);
 }
 
 void	dl_list_iter(t_dl_list *head, void (*f)(t_dl_list *, void *), void *arg)
@@ -80,6 +79,18 @@ void	dl_list_iter(t_dl_list *head, void (*f)(t_dl_list *, void *), void *arg)
 	{
 		f(head, arg);
 		head = head->next;
+	}
+}
+
+void	dl_list_clear(t_dl_list **head, void (*del)(void *))
+{
+	t_dl_list	*tmp;
+
+	while (head && *head)
+	{
+		tmp = (*head)->next;
+		dl_list_delone(*head, del);
+		*head = tmp;
 	}
 }
 
@@ -94,12 +105,12 @@ typedef struct s_uint64_entry
 
 typedef struct s_uint64_map
 {
-	t_uint64_entry	*entries;
+	t_dl_list		**entries;
 	uint32_t		len;
 	uint32_t		cap;
 }	t_uint64_map;
 
-bool	map_create(t_uint64_map *map, uint32_t cap)
+bool	uint64_map_create(t_uint64_map *map, uint32_t cap)
 {
 	cap--;
 	cap |= cap >> 1;
@@ -112,29 +123,37 @@ bool	map_create(t_uint64_map *map, uint32_t cap)
 		return (false);
 	map->cap = cap;
 	map->len = 0;
-	map->entries = calloc(sizeof(t_uint64_entry), cap);
+	map->entries = calloc(sizeof(t_dl_list *), cap);
 	return (map->entries != NULL);
 }
 
-void	map_destroy(t_uint64_map *map)
+void	uint64_map_destroy(t_uint64_map *map)
 {
 	if (__builtin_expect(!map, 0))
 		return ;
+	while (map->cap--)
+		dl_list_clear(map->entries + map->cap, NULL);
 	free(map->entries);
-	map->len = 0;
-	map->cap = 0;
 	map->entries = NULL;
+	map->len = 0;
 }
 
-void	*map_get(t_uint64_map *map, uint64_t key)
+void	*uint64_map_get(t_uint64_map *map, uint64_t key)
 {
-	uint64_t	index;
-	t_u
-	
+	t_dl_list		*entries;
+	t_uint64_entry	*entry;
+
 	if (__builtin_expect(!map, 0))
 		return (NULL);
-	index = key & (map->cap - 1);
-	
+	entries = map->entries[key & (map->cap - 1)];
+	while (entries)
+	{
+		entry = (t_uint64_entry *)entries->data;
+		if (entry->key == key)
+			return (entry->value);
+		entries = entries->next;
+	}
+	return (NULL);
 }
 
 typedef struct s_free_list
@@ -162,77 +181,6 @@ typedef struct s_ecs
 	uint16_t		comp_size[63];
 	uint8_t			comp_nb;
 }	t_ecs;
-
-t_uint64_map	*uint64_map_create(void)
-{
-	t_uint64_map	*map;
-	uint16_t		cap;
-
-	cap = ECS_UINT64_MAP_CAP - 1;
-	cap |= cap << 1;
-	cap |= cap << 2;
-	cap |= cap << 4;
-	cap |= cap << 8;
-	cap++;
-	map = malloc(sizeof(t_uint64_map));
-	if (__builtin_expect(!map, 0))
-		return (NULL);
-	map->cap = cap;
-	map->count = 0;
-	memset(map->entries, 0, cap * sizeof(t_uint64_entry));
-	return (map);
-}
-
-void	uint64_map_destroy(t_uint64_map *map, void (*del)(void *))
-{
-	uint64_t	i;
-
-	if (!map)
-		return ;
-	i = -1;
-	while (del && ++i < map->count)
-		if (map->entries[i].value)
-			del(map->entries[i].value);
-	free(map);
-}
-
-void	*uint64_map_get(t_uint64_map *map, uint64_t key)
-{
-	uint64_t	index;
-	uint64_t	start;
-
-	if (__builtin_expect(!key || !map || !map->count, 0))
-		return (NULL);
-	index = key & (map->cap - 1);
-	start = index;
-	while (map->entries[index].key != 0)
-	{
-		if (map->entries[index].key == key)
-			return (map->entries[index].value);
-		index = (index + 1) & (map->cap - 1);
-		if (index == start)
-			return (NULL);
-	}
-	return (NULL);
-}
-
-void	uint64_map_set(t_uint64_map	*map, uint64_t key, void *data)
-{
-	uint64_t	index;
-
-	if (__builtin_expect(!key || !map || map->count >= map->cap, 0))
-		return ;
-	index = key & (map->cap - 1);
-	while (map->entries[index].key != 0)
-	{
-		if (map->entries[index].key == key)
-			map->entries[index].value = data;
-		index = (index + 1) & (map->cap - 1);
-	}
-	map->entries[index].key = key;
-	map->entries[index].value = data;
-	map->count++;
-}
 
 t_ecs	*ecs_create(uint8_t nb, uint16_t *comps)
 {
